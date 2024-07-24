91a11c59bc8b5eca51cd43da57b65dd926eaceba
Sohag Jabed <sohagjabed10@gmail.com>
2024-07-24 17:33:47 +0530

    Enhanced /search api

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -6,7 +6,10 @@ const API_KEY = process.env.API_KEY;
 export async function GET(req: NextRequest) {
 	const searchTerm = req.nextUrl.searchParams.get('q');
     console.log("searchTerm :", searchTerm);
-    const url = `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${API_KEY}&pageSize=10&searchIn=title`;
+    const date = new Date();
+    date.setDate(date.getDate() - 2);
+    const from = date.toISOString();
+    const url = `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=${API_KEY}&pageSize=10&searchIn=title&from=${from}`;
     const response = await fetch(url);
 	const data: unknown = await response.json();
     return NextResponse.json(data);
