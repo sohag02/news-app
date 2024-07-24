57aaf899ff02a1aa3c5b0bb8601784c6021a2b14
Sohag Jabed <sohagjabed10@gmail.com>
2024-07-24 19:15:32 +0530

    added Suspense for Data fetching

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,21 +3,22 @@ import React, { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { Article } from "@/components/article";
 import { useState } from "react";
+import { Suspense } from "react";
 
 interface Article {
-  title: string;
-  description: string;
-  url: string;
-  urlToImage: string;
-  publishedAt: string;
-  source: {
-    name: string;
+  title?: string;
+  description?: string;
+  url?: string;
+  urlToImage?: string;
+  publishedAt?: string;
+  source?: {
+    name?: string;
   };
 }
 
 interface Data {
-  totalResults: number;
-  articles: Article[];
+  totalResults?: number;
+  articles?: Article[];
 }
 
 export default function SearchPage() {
@@ -30,7 +31,7 @@ export default function SearchPage() {
     const response = await fetch("/api/search?q=" + searchTerm);
     const data = await response.json() as Data
     console.log("data", data);
-    return data.articles;
+    return data.articles ?? [];
   };
 
   useEffect(() => {
@@ -40,33 +41,24 @@ export default function SearchPage() {
     .catch((error) => console.log(error));
   }, [searchTerm]);
 
-  // useEffect(() => {
-  //   fetch("/api/search?q=" + searchTerm)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log("data", data);
-  //       setArticles(data.articles);
-  //     })
-  //     .catch((error) => console.log(error));
-  // }, [searchTerm]);
-
-  console.log("articles", articles);
 
   return (
-    <div className="container mx-auto px-4">
-      <div className="mt-4 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-        {articles.map((article, index) => (
-          <Article
-            key={index}
-            title={article?.title}
-            description={article?.description}
-            image={article?.urlToImage}
-            url={article?.url}
-            pub={article?.publishedAt}
-						source={article?.source?.name}
-          />
-        ))}
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="container mx-auto px-4">
+        <div className="mt-4 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
+          {articles.map((article, index) => (
+            <Article
+              key={index}
+              title={article?.title ?? ""}
+              description={article?.description ?? ""}
+              image={article?.urlToImage ?? ""}
+              url={article?.url ?? ""}
+              pub={article?.publishedAt ?? ""}
+              source={article?.source?.name ?? ""}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
